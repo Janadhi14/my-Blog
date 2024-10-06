f4c909431688bc1749ec27a1eb25c891cbec0ed5
Janadhi14 <janadhi14@gmail.com>
2024-10-06 23:11:03 +1300

    name updated

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Janadhi's Blog  🐿️",
+    pageTitle: "Janadhi's Knowledge Base  🐿️",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
