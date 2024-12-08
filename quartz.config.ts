67b19c04cc732b84059eb9f7f230500081f2477d
Janadhi Dissanayake <disja508@student.otago.ac.nz>
2024-12-08 14:12:04 +1300

    updated title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Janadhi's Knowledge Base 🪼",
+    pageTitle: "Janadhi's Blog 🪼",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
